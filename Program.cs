fcad8d7252a38f5fe085ec866a48e98323922bbf
nevinhall <nevinhall98@gmail.com>
2021-03-26 13:46:57 +0000

    Define project infrastructure.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Vuturetest
 {
@@ -6,14 +11,41 @@ namespace Vuturetest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            String resLetterOccurence = LetterOccurence("I have some cheese", "e");
+
+            String resIsPalindrome = IsPalindrome("I have some cheese");
+            String resIsPalindrome1 = IsPalindrome("God saved Eva’s dog");
+
+            string[] arr = { "dog", "cat", "large" };
+            string resCensoredWord = CensoredWord(arr, "I have a cat named Meow and a dog name Woof. I love the dog a lot. He is larger than a small horse.");
+
+            Console.WriteLine("Result Letter Occurence: \n" + resLetterOccurence);
+            Console.WriteLine("\nResult is Palindrome: \n"+ resIsPalindrome);
+            Console.WriteLine("\nResult is Palindrome: \n"+ resIsPalindrome1);
+            Console.WriteLine("\nResult is Censord Word: \n" + resCensoredWord);
+
         }
 
 
-       
-        public String letter_occurence(String senetence, String leter)
+
+        public static String LetterOccurence(String senetence, String letter)
         {
-            re
+            return "letter_occurence";
+        }
+
+ 
+        public static String IsPalindrome(String senetence)
+        {
+            return "IsPalindrome";
+        }
+
+
+
+        public static String CensoredWord(string[] arr, String senetence) {
+            Dictionary<string, int> censoredWords;
+        
+            return "sensoredword";
         }
     }
 }
