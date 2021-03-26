d8b3e0d164bf7c969acc1cd4cfbc952e743e73d4
nevinhall <nevinhall98@gmail.com>
2021-03-26 13:03:15 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Vuturetest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+       
+        public String letter_occurence(String senetence, String leter)
+        {
+            re
+        }
+    }
+}
+
+
